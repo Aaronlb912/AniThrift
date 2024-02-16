cdc1620686f4310a02adf97a553882506587fef9
007jedgar <007j.edgar@gmail.com>
2024-02-16 11:42:21 -0600

    styles: add base colors

---
+++ src/ui/colors.js
@@ -0,0 +1,9 @@
+// colors.js
+const colors = {
+  black: '#0D0D0D',
+  white: '#F8F8F8',
+  grey: '#212121',
+  lightGrey: '#D3D3D3',
+};
+
+export default colors;
---
+++ src/ui/typo.js
@@ -0,0 +1,15 @@
+// typography.js
+import colors from './colors';
+
+const typographyStyles = {
+  header: {
+    color: colors.black,
+    // other styles
+  },
+  body: {
+    color: colors.grey,
+    // other styles
+  },
+};
+
+export default typographyStyles;
