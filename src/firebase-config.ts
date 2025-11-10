4a59df7a8d643feb0a14aaced6c61cc14176b3f0
Aaronlb912 <abryant912@hotmail.com>
2025-11-10 09:54:45 -0500

    hid firebase key

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,29 +1,41 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+import { getFunctions } from "firebase/functions";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBm07IEdU_vL-Qw5V1n_a-ocUnNALiHUTg",
-  authDomain: "anithrift-e77a9.firebaseapp.com",
-  projectId: "anithrift-e77a9",
-  storageBucket: "anithrift-e77a9.appspot.com",
-  messagingSenderId: "1094057328928",
-  appId: "1:1094057328928:web:98da67c646c7965fa15745",
-  measurementId: "G-TV1DPFSM7S",
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+const requiredKeys: Array<keyof FirebaseOptions> = [
+  "apiKey",
+  "authDomain",
+  "projectId",
+  "storageBucket",
+  "messagingSenderId",
+  "appId",
+];
+
+const missingKeys = requiredKeys.filter(
+  (key) => !firebaseConfig[key]
+);
+
+if (missingKeys.length > 0) {
+  throw new Error(
+    `Missing Firebase configuration values: ${missingKeys.join(
+      ", "
+    )}. Please add them to your environment variables.`
+  );
+}
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
